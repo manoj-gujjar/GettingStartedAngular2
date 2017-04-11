ea5bf667b05075d0b3cbf41107ffc918aa6f7527
Gujjar <337506@cognizant.com>
2017-04-11 20:09:26 +0530

    Added Files

---
+++ APM/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'pm-app',
+    template: `
+        <h1>Angular2: Getting Started in My Project fdsdfgsdfg</h1>
+    `
+})
+export class AppComponent { }
